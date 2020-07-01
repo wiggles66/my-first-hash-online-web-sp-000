c097801434eea7aeacbfa8e1a1cfebcd901dbe22
Michael Waide <mvwaide@gmail.com>
2020-07-01 18:54:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,10 +25,10 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-hash["oil paintings"]
-end
 
+
+end
+hash["oil paintings"]
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
