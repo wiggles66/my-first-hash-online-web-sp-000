74453f55d048b49c45da58fb3a42e9e42cd83756
Michael Waide <mvwaide@gmail.com>
2020-07-01 18:37:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-the_manifest = my_hash
+the_manifest = the_hash
 
 end
 
@@ -41,3 +41,4 @@ def adding
   # return the shipping_manifest hash below
 
 end
+""
