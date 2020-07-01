de55d80fd5fadc853b2e27f7ef12cf7aba26d911
Michael Waide <mvwaide@gmail.com>
2020-07-01 19:07:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,17 +31,16 @@ end
 def adding
   shipping_manifest = {
 
-    "whale bone corsets" => 5
-"porcelain vases" => 2
-"oil paintings" => 3
-"muskets" => 2
-"gun powder" => 4
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
                }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest[:"muskets"] => 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
+shipping_manifest[:"gun powder"] => 4
 
   # return the shipping_manifest hash below
 hash
